64fb5559af218ad3a877f37664f9eca9ecfc20ca
Liam <liammehlex@gmail.com>
2023-12-26 16:47:22 +0100

    misplaced comment

--- src/global_config.hpp
+++ src/global_config.hpp
@@ -1,5 +1,9 @@
 #pragma once
 #include <cstdint>
+/*
+    Base rate at which everything refreshes
+    This program is based on polling. This is the base rate, mainly setting the rate a which the display refreshes.
+*/
 int constexpr global_base_rate = 60;
 constexpr std::size_t WIDTH  = 800;
 constexpr std::size_t HEIGHT = 600;

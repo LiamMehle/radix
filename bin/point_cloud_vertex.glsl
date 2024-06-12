11e5d267fa4e60751d1ccd1f74fe018823ad2096
Liam Mehle <liammehlex@gmail.com>
2024-06-12 14:42:29 +0200

    higher zoom to fill the screen

--- bin/point_cloud_vertex.glsl
+++ bin/point_cloud_vertex.glsl
@@ -3,5 +3,5 @@ layout (location=0) in vec3 pos;
 out float heights;
 void main() {
     heights = pos.z;
-    gl_Position = vec4(pos.xy * 2, pos.z, 1.);
+    gl_Position = vec4(pos.xy * 4, pos.z, 1.);
 }
